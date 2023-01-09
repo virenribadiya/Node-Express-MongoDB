9a23697d7f3a266d4f4d8824d38fa97cc6dbd5fa
virenribadiya <vribadiya278@rku.ac.in>
2023-01-09 16:29:45 +0530

    Very first routes

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -9,6 +9,18 @@ app.get("/",function(request,response){
 
 })
 
+app.get("/contact",function(request,response){
+    //console.log(request);
+    response.send("contact me: vribadiya278@rku.ac.in");
+
+})
+
+app.get("/about",function(request,response){
+    //console.log(request);
+    response.send("My name is Viren Ribadiya. I love code and Coffee!");
+
+})
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
     // when our browser try to get in touch with our server then it didnt get anything back. so it says get error.
